cc50207c683483f9ca5c9627f8a740baff58c4c5
HoshiriAki <ash_cxy@qq.com>
2024-07-16 21:35:24 +0800

    20240716

--- src/config.ts
+++ src/config.ts
@@ -36,7 +36,7 @@ export const navBarConfig: NavBarConfig = {
     LinkPreset.About,
     {
       name: 'GitHub',
-      url: 'https://github.com/saicaca/fuwari', // Internal links should not include the base path, as it is automatically added
+      url: 'https://github.com/HoshiriAki', // Internal links should not include the base path, as it is automatically added
       external: true, // Show an external link icon and will open in a new tab
     },
   ],
@@ -44,7 +44,7 @@ export const navBarConfig: NavBarConfig = {
 
 export const profileConfig: ProfileConfig = {
   avatar: 'assets/images/demo-avatar.png', // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-  name: 'Lorem Ipsum',
+  name: 'Hoshimi Aki',
   bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
   links: [
     {
@@ -62,7 +62,7 @@ export const profileConfig: ProfileConfig = {
     {
       name: 'GitHub',
       icon: 'fa6-brands:github',
-      url: 'https://github.com/saicaca/fuwari',
+      url: 'https://github.com/HoshiriAki',
     },
   ],
 }
