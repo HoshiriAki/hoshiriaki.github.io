aefab81579f845d8fd95130006ebe2223f3ec51a
HoshiriAki <ash_cxy@qq.com>
2024-07-16 21:42:10 +0800

    20240716

--- src/config.ts
+++ src/config.ts
@@ -15,17 +15,21 @@ export const siteConfig: SiteConfig = {
     fixed: false, // Hide the theme color picker for visitors
   },
   banner: {
-    enable: false,
+    enable: true,
     src: 'assets/images/demo-banner.png', // Relative to the /src directory. Relative to the /public directory if it starts with '/'
     position: 'center', // Equivalent to object-position, defaults center
   },
   favicon: [
-    // Leave this array empty to use the default favicon
-    // {
-    //   src: '/favicon/icon.png',    // Path of the favicon, relative to the /public directory
-    //   theme: 'light',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
-    //   sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
-    // }
+    {
+      src: '/favicon/icon_l.png', // Path of the favicon, relative to the /public directory
+      theme: 'light', // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
+      sizes: '128x128', // (Optional) Size of the favicon, set only if you have favicons of different sizes
+    },
+    {
+      src: '/favicon/icon_d.png', // Path of the favicon, relative to the /public directory
+      theme: 'dark', // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
+      sizes: '128x128', // (Optional) Size of the favicon, set only if you have favicons of different sizes
+    },
   ],
 }
 
