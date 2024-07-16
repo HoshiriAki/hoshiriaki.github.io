e9195ff4397ea2a8f953e85adc71efa21859347d
HoshiriAki <ash_cxy@qq.com>
2024-07-16 22:13:16 +0800

    20240716

--- src/config.ts
+++ src/config.ts
@@ -48,7 +48,7 @@ export const navBarConfig: NavBarConfig = {
 
 export const profileConfig: ProfileConfig = {
   avatar: 'assets/images/demo-avatar.png', // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-  name: '秋星海(あき　ほしみ)',
+  name: '秋星海(あき_ほしみ)',
   bio: ' Go to the future',
   links: [
     {
