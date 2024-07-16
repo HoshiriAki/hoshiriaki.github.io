fff53708af80d938c77ac2a461a9b5d95211a679
HoshiriAki <ash_cxy@qq.com>
2024-07-16 21:19:46 +0800

    20240716

--- astro.config.mjs
+++ astro.config.mjs
@@ -30,7 +30,7 @@ const oklchToHex = str => {
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://hoshiriaki.github.io',
+  site: 'https://blog.project-hoshizora.cn',
   base: '/',
   trailingSlash: 'always',
   integrations: [
